6b5e371bf0ddc7b179ff36ddd8b12a384a73fa3c
Moritz <fullermoritz@gmail.com>
2022-06-29 16:05:22 +0200

    🐛 add username & password to mongoose

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 mongoose
-    .connect(process.env.MONGO_URL!)
+    .connect(process.env.MONGO_URL!, {
+        auth: {
+            username: process.env.MONGO_USER!,
+            password: process.env.MONGO_PASS!,
+        },
+    })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Could not connect to MongoDB", err));
