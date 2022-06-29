bef52df79b1ca218f55b179daaefcb72cfc9201f
Moritz <fullermoritz@gmail.com>
2022-06-29 16:37:55 +0200

    🔥 removed logging

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-console.log(process.env.MONGO_USER);
-console.log(process.env.MONGO_PASS);
-
 mongoose
     .connect(process.env.MONGO_URL!)
     .then(() => console.log("Connected to MongoDB"))
