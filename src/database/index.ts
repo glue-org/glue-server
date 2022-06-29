3dfdf612e4d19795f66e725b114eca752f2ab9b9
Moritz <fullermoritz@gmail.com>
2022-06-29 16:26:24 +0200

    🩹 use url connection string

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,11 +4,6 @@ console.log(process.env.MONGO_USER);
 console.log(process.env.MONGO_PASS);
 
 mongoose
-    .connect(process.env.MONGO_URL!, {
-        auth: {
-            username: process.env.MONGO_USER!,
-            password: process.env.MONGO_PASS!,
-        },
-    })
+    .connect(process.env.MONGO_URL!)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Could not connect to MongoDB", err));
